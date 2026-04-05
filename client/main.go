2b32fa2eb7f5e8b4071597ff7b92ea1002313428
genus555 <yunxiaoche@gmail.com>
2026-04-04 19:43:12 -0700

    generate encryption key if new user

--- client/main.go
+++ client/main.go
@@ -1,3 +1,25 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	startup "github.com/genus555/spa/internal/init"
+)
+
+func main() {
+	fmt.Println("Welcome to the Simple Password Aggregator")
+	godotenv.Load()
+	if err := startup.GenerateEnv(); err != nil {
+		log.Fatalf("Problem creating .env: %v", err)
+	}
+	key, err := startup.DecodeKey(os.Getenv("ENCRYPTION_KEY"))
+	if err != nil {
+		log.Fatalf("Problem getting encryption key: %v", err)
+	}
+	fmt.Println(key)
+
+	fmt.Println("Insert 2 factor here")
+}
